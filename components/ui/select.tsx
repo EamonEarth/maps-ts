3dd16542523dbdf846ed54ac1f24df6844362619
Eamon Travers <eamon.trav@gmail.com>
2024-08-14 13:56:35 +0200

    radix select primitive hackaround

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -73,7 +73,16 @@ const SelectContent = React.forwardRef<
 >(({ className, children, position = "popper", ...props }, ref) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
-      ref={ref}
+      ref={(node) => {
+        if (node) {
+          node.ontouchend = (e) => e.preventDefault(); // Prevent touch events from propagating
+        }
+        if (ref && typeof ref === 'function') {
+          ref(node);
+        } else if (ref && 'current' in ref) {
+          ref.current = node;
+        }
+      }}
       onClick={(event:React.MouseEvent)=>event?.stopPropagation()}
       className={cn(
         "relative z-[70] max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
